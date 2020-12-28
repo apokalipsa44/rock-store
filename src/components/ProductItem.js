b684a596585b59b1774a79e27800d6a41b90b622
apokalipsa44 <m_lachowicz@wp.pl>
2020-12-28 15:15:41 +0100

    set product quantity done

--- src/components/ProductItem.js
+++ src/components/ProductItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
@@ -6,8 +6,10 @@ import CardContent from "@material-ui/core/CardContent";
 import CardMedia from "@material-ui/core/CardMedia";
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
+import { Box, Grid } from "@material-ui/core";
 
 function ProductItem({ product }) {
+  const [quantity, setQuantity] = useState(0);
   console.log(product);
 
   return (
@@ -31,9 +33,37 @@ function ProductItem({ product }) {
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Button size="small" color="primary">
-          BUY
-        </Button>
+        <Grid container xs={12}>
+          <Grid item>
+            <div>
+              <Button
+                display="inline"
+                onClick={() => setQuantity(quantity + 1)}
+              >
+                +
+              </Button>
+              <Typography
+                display="inline"
+                variant="body1"
+                color="textSecondary"
+                component="p"
+              >
+                {quantity}
+              </Typography>
+              <Button
+                display="inline"
+                onClick={() => setQuantity(quantity - 1)}
+              >
+                -
+              </Button>
+            </div>
+          </Grid>
+          <Grid item>
+            <Button size="small" color="primary">
+              BUY
+            </Button>
+          </Grid>
+        </Grid>
       </CardActions>
     </Card>
   );
---
+++ src/views/CartDrawer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CartDrawer() {
+  return <div>cardmdrawer</div>;
+}
+
+export default CartDrawer;
