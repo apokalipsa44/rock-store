bf2eb34e265f4823a03c5f55c20f8507a7f2be8a
apokalipsa44 <m_lachowicz@wp.pl>
2021-01-11 15:47:11 +0100

    checkout summary done

--- src/components/Checkout/CheckoutSummary.js
+++ src/components/Checkout/CheckoutSummary.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import { Typography, Button, Fade, Container, Box } from "@material-ui/core";
+import React from "react";
 
 function CheckoutSummary() {
-    return (
+  const handleOnClick = () => {
+      
+  };
+  return (
+    <Container>
+      <Fade timeout={2000} in>
         <div>
-            thanks for shopping!
+          <Typography gutterBottom variant="h4">Thanks for shopping</Typography>
         </div>
-    )
+      </Fade>
+
+      <Fade timeout={3500} in>
+        <Box m={3}>
+          <Button variant="contained" size='large' onClick={handleOnClick}>Back to the store</Button>
+        </Box>
+      </Fade>
+    </Container>
+  );
 }
 
-export default CheckoutSummary
+export default CheckoutSummary;
